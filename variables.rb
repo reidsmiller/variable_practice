fbfa835bcc1586c07da9ccc773203de19c6867b9
Reid Miller <reid.s.miller@gmail.com>
2023-01-03 13:52:19 -0700

    Change string variables and add puts statements

--- variables.rb
+++ variables.rb
@@ -10,10 +10,19 @@ miles_year = 15000
 
 # Float variables
 bank_money = 12345.67
-ave_mileage = 28.7
+ave_mileage = 60.7
 ave_speed = 42.4
 
 # Boolean variables
 at_home = true
 tired = false
 hungry = true
+
+# Change string variables
+name = "Reid Bond"
+birthday = "03/01/1901"
+fav_food = "Dirt"
+
+puts "#{name} was born on #{birthday}. He likes to eat #{fav_food}."
+puts "He is #{age} years old and has owned #{cars_owned} cars in his lifetime."
+puts "He drives on average #{ave_mileage} miles per day at a rate of #{ave_speed} miles per hour for a total of #{miles_year}."
